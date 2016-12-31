b5b209552b2cf9d0c81207322fa34c49e2916555
Samantha Doran <samanthadoran3@gmail.com>
2016-12-30 21:15:11 -0800

    Adds a helper function to load a rom from a filepath.

--- console.lisp
+++ console.lisp
@@ -1,7 +1,7 @@
 (defpackage #:psx-console
   (:nicknames #:psx)
   (:use :cl :psx-cpu)
-  (:export #:make-psx))
+  (:export #:make-psx #:load-rom-from-file))
 
 (in-package :psx-console)
 (declaim (optimize (speed 3) (safety 1)))
@@ -12,3 +12,9 @@
   (bios-rom
    (make-array #x8000 :element-type '(unsigned-byte 8) :initial-element 0)
    :type (simple-array (unsigned-byte 8) (#x8000))))
+
+(defun load-rom-from-file (filepath)
+  (with-open-file (stream filepath :element-type '(unsigned-byte 8))
+    (let ((rom (make-array (file-length stream) :element-type '(unsigned-byte 8))))
+      (read-sequence rom stream)
+      rom)))
