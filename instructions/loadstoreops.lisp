30349fe4f5718e63c2ce966aded1191df8c014a7
Samantha Doran <samanthadoran3@gmail.com>
2017-11-19 16:38:34 -0500

    Sort instructions in loadstoreops

--- instructions/loadstoreops.lisp
+++ instructions/loadstoreops.lisp
@@ -9,28 +9,6 @@
    (aref (cpu-registers cpu) target-register)
    (ash immediate 16)))
 
-; TODO(Samantha): Consider making this io a bit more generic, it's
-; frustrating to repeat myself.
-(def-r-type mfc0 #xC0000
-  (set-register cpu target-register
-    (case destination-register
-      (12 (cpu-status-register cpu))
-      (otherwise
-       (format t "Unknown read to cop0$~d~%" destination-register)
-       0))))
-
-(def-r-type mtc0 #xC0004
-  (case destination-register
-    (12
-     (setf
-      (cpu-status-register cpu)
-      (aref (cpu-registers cpu) target-register)))
-    (otherwise
-     (format t "Unknown write of 0x~8,'0X to cop0$~d~%"
-             (aref (cpu-registers cpu) target-register)
-             destination-register)
-     0)))
-
 ; TODO(Samantha): Consider making a macro for cache sensitivity.
 (def-i-type lb #x20
   (when (not (is-cache-isolated cpu))
@@ -89,16 +67,6 @@
         (sign-extend immediate)
         (aref (cpu-registers cpu) source-register)))))))
 
-(def-i-type sw #x2B
-  (when (not (is-cache-isolated cpu))
-    (write-cpu-word
-     cpu
-     (wrap-word
-      (+
-       (sign-extend immediate)
-       (aref (cpu-registers cpu) source-register)))
-     (aref (cpu-registers cpu) target-register))))
-
 (def-i-type sb #x28
   (when (not (is-cache-isolated cpu))
     (write-cpu
@@ -118,6 +86,38 @@
        (sign-extend immediate)
        (aref (cpu-registers cpu) source-register)))
      (ldb (byte 16 0) (aref (cpu-registers cpu) target-register)))))
+
+(def-i-type sw #x2B
+  (when (not (is-cache-isolated cpu))
+    (write-cpu-word
+     cpu
+     (wrap-word
+      (+
+       (sign-extend immediate)
+       (aref (cpu-registers cpu) source-register)))
+     (aref (cpu-registers cpu) target-register))))
+
+; TODO(Samantha): Consider making this io a bit more generic, it's
+; frustrating to repeat myself.
+(def-r-type mfc0 #xC0000
+  (set-register cpu target-register
+                (case destination-register
+                  (12 (cpu-status-register cpu))
+                  (otherwise
+                   (format t "Unknown read to cop0$~d~%" destination-register)
+                   0))))
+
+(def-r-type mtc0 #xC0004
+  (case destination-register
+    (12
+     (setf
+      (cpu-status-register cpu)
+      (aref (cpu-registers cpu) target-register)))
+    (otherwise
+     (format t "Unknown write of 0x~8,'0X to cop0$~d~%"
+             (aref (cpu-registers cpu) target-register)
+             destination-register)
+     0)))
 
 (def-r-type mfhi #xFF10
   (set-register
