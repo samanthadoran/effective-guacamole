e60ae89626dc65e1180aba4d6be42ae4398769d8
Samantha Doran <samanthadoran3@gmail.com>
2017-11-22 19:53:24 -0500

    Finally add address masking to the mmu. Add other locations for reading/writing.

--- mmu.lisp
+++ mmu.lisp
@@ -2,19 +2,47 @@
 
 (declaim (optimize (speed 3) (safety 1)))
 
+(declaim (ftype (function ((unsigned-byte 32))
+                          (unsigned-byte 32))
+                mask-address))
+(defun mask-address (address)
+  "While the PSX has segments of memory, there is no proper virtual memory and
+   we only really care about the segment if it's out of a cached region.
+   Performs a bit mask of the address so we can access items in mirrored areas
+   without too much hassle."
+  ; TODO(Samantha): This could certainly be done with a lookup table, but I'm
+  ; not certain it would be clearer.
+  (cond
+    ; If bits [31:29] are greater than 6, we're in kseg2. The only things here
+    ; are some io registers and other miscelanneous things, no need to mask off
+    ; bits.
+    ((>= (ldb (byte 3 29) address) 6)
+     address)
+    ; If bits [31:29] are equal to 5, we're in kseg1
+    ((= (ldb (byte 3 29) address) 5)
+     (ldb (byte 29 0) address))
+    ; If bits [31:29] are equal to 4, we're in kseg0
+    ((= (ldb (byte 3 29) address) 4)
+     (ldb (byte 31 0) address))
+    ; Otherwise, we're in kuseg
+    (t address)))
+
 (defconstant mirror-size #x20000000)
 (defconstant kuseg-base #x00000000)
 (defconstant kseg0-base #x80000000)
 (defconstant kseg1-base #xA0000000)
-(defconstant ram-begin-kseg1 #xA0000000)
+(defconstant ram-begin #x00000000)
 (defconstant ram-size #x200000)
-(defconstant bios-begin-address-kseg1 #xBFC00000)
+(defconstant bios-begin-address #x1FC00000)
 (defconstant memory-control-begin #x1F801000)
 (defconstant memory-control-size 36)
-(defconstant expansion-base-1-address #x1F000000)
-(defconstant expansion-base-2-address #x1F802000)
+(defconstant expansion-1-begin #x1F000000)
+(defconstant expansion-2-begin #x1F802000)
+(defconstant expansion-2-size 66)
 (defconstant ram-size-begin #x1F801060)
 (defconstant cache-control #xFFFE0130)
+(defconstant spu-registers-begin #x1F801C00)
+(defconstant spu-registers-size 640)
 
 (declaim (ftype (function ((unsigned-byte 32)
                            (unsigned-byte 32)
@@ -90,88 +118,98 @@
                 load-word*))
 (defun load-word* (psx address)
   ; TODO(Samantha): Implement more places, simplify the cond.
-  (cond
-    ; BIOS
-    ((in-range bios-begin-address-kseg1
-               (array-dimension (psx-bios-rom psx) 0)
-               address)
-     (read-word-from-byte-array
-      (psx-bios-rom psx) (- address bios-begin-address-kseg1)))
-    ; RAM
-    ((in-range ram-begin-kseg1 ram-size address)
-     (read-word-from-byte-array (psx-ram psx) (- address ram-begin-kseg1)))
-    ; Unimplemented.
-    (t (error "Word reads to 0x~8,'0X are unimplemented~%" address))))
+  (let ((address (mask-address address)))
+    (cond
+      ; BIOS
+      ((in-range bios-begin-address
+                 (array-dimension (psx-bios-rom psx) 0)
+                 address)
+       (read-word-from-byte-array
+        (psx-bios-rom psx) (- address bios-begin-address)))
+      ; RAM
+      ((in-range ram-begin ram-size address)
+       (read-word-from-byte-array (psx-ram psx) (- address ram-begin)))
+      ; Unimplemented.
+      (t (error "Word reads to 0x~8,'0X are unimplemented~%" address)))))
 
 ; TODO(Samantha): Figure out a way to fix this shadowing.
 (declaim
  (ftype (function (psx (unsigned-byte 32) (unsigned-byte 8)) (unsigned-byte 8))
         write-byte*))
 (defun write-byte* (psx address value)
-  (declare (ignore psx value))
-  (cond
-    ; Unimplemented.
-    (t (error "Byte writes to 0x~8,'0X are unimplemented!~%" address))))
+  (declare (ignore psx))
+  (let ((address (mask-address address)))
+    (cond
+      ((in-range expansion-2-begin expansion-2-size address)
+       (format t "Wrote 0x~8,'0x to expansion2 @ 0x~8,'0x!~%" value address)
+       value)
+      ; Unimplemented.
+      (t (error "Byte writes to 0x~8,'0X are unimplemented!~%" address)))))
 
 (declaim
  (ftype (function (psx (unsigned-byte 32) (unsigned-byte 16)) (unsigned-byte 16))
         write-half-word*))
 (defun write-half-word* (psx address value)
-  (declare (ignore psx value))
-  (cond
-    ; Unimplemented.
-    (t (error "Half-word writes to 0x~8,'0X are unimplemented!~%" address))))
+  (declare (ignore psx))
+  (let ((address (mask-address address)))
+    (cond
+      ((in-range spu-registers-begin spu-registers-size address)
+       (format t "Wrote 0x~8,'0x to spu @ 0x~8,'0x!~%" value address)
+       value)
+      ; Unimplemented.
+      (t (error "Half-word writes to 0x~8,'0X are unimplemented!~%" address)))))
 
 (declaim
  (ftype (function (psx (unsigned-byte 32) (unsigned-byte 32)) (unsigned-byte 32))
         write-word*))
 (defun write-word* (psx address value)
-  (cond
-    ((in-range memory-control-begin memory-control-size address)
-     (cond
-       ; Expansion base 1 register
-       ((= address memory-control-begin)
-        (when (/= value expansion-base-1-address)
-          (error "Unexpected value for expansion-base-1-address, got 0x~8,'0x, expected 0x~8,'0x!~%" value expansion-base-1-address))
-        value)
-       ((= address (+ memory-control-begin 4))
-        (when (/= value expansion-base-2-address)
-          (error "Unexpected value for expansion-base-2-address, got 0x~8,'0x, expected 0x~8,'0x!~%" value expansion-base-2-address))
-        value)
-       ((= address (+ memory-control-begin 8))
-        (format t "Wrote 0x~8,'0x to expansion 1 delay/size!~%" value)
-        value)
-       ((= address (+ memory-control-begin #xC))
-        (format t "Wrote 0x~8,'0x to expansion 3 delay/size!~%" value)
-        value)
-       ((= address (+ memory-control-begin #x10))
-        (format t "Wrote 0x~8,'0x to bios delay/size!~%" value)
-        value)
-       ((= address (+ memory-control-begin #x14))
-        (format t "Wrote 0x~8,'0x to spu_delay!~%" value)
-        value)
-       ((= address (+ memory-control-begin #x18))
-        (format t "Wrote 0x~8,'0x to cdrom_delay!~%" value)
-        value)
-       ((= address (+ memory-control-begin #x1C))
-        (format t "Wrote 0x~8,'0x to expansion 2 delay/size!~%" value)
-        value)
-       ((= address (+ memory-control-begin #x20))
-        (format t "Wrote 0x~8,'0x to common delay!~%" value)
-        value)
-       (t (error "Unexpected write of 0x~8,'0x! to Memory Control at 0x~8,'0x!~%" value address))))
-    ((= address ram-size-begin)
-     (format t "Wrote 0x~8,'0x to ram size!~%" value)
-     value)
-    ((= address cache-control)
-     (format t "Wrote 0x~8,'0x to cache control!~%" value)
-     value)
-    ; RAM
-    ((in-range ram-begin-kseg1 ram-size address)
-     (format t "Wrote 0x~8,'0x to ram(0x~8,'0X)!~%" value address)
-     (write-word-to-byte-array (psx-ram psx) (- address ram-begin-kseg1) value))
-    ; Unimplemented.
-    (t (error "Word writes to 0x~8,'0X are unimplemented!~%" address))))
+  (let ((address (mask-address address)))
+    (cond
+      ((in-range memory-control-begin memory-control-size address)
+       (cond
+         ; Expansion base 1 register
+         ((= address memory-control-begin)
+          (when (/= value expansion-1-begin)
+            (error "Unexpected value for expansion-1-begin, got 0x~8,'0x, expected 0x~8,'0x!~%" value expansion-1-begin))
+          value)
+         ((= address (+ memory-control-begin 4))
+          (when (/= value expansion-2-begin)
+            (error "Unexpected value for expansion-2-begin, got 0x~8,'0x, expected 0x~8,'0x!~%" value expansion-2-begin))
+          value)
+         ((= address (+ memory-control-begin 8))
+          (format t "Wrote 0x~8,'0x to expansion 1 delay/size!~%" value)
+          value)
+         ((= address (+ memory-control-begin #xC))
+          (format t "Wrote 0x~8,'0x to expansion 3 delay/size!~%" value)
+          value)
+         ((= address (+ memory-control-begin #x10))
+          (format t "Wrote 0x~8,'0x to bios delay/size!~%" value)
+          value)
+         ((= address (+ memory-control-begin #x14))
+          (format t "Wrote 0x~8,'0x to spu_delay!~%" value)
+          value)
+         ((= address (+ memory-control-begin #x18))
+          (format t "Wrote 0x~8,'0x to cdrom_delay!~%" value)
+          value)
+         ((= address (+ memory-control-begin #x1C))
+          (format t "Wrote 0x~8,'0x to expansion 2 delay/size!~%" value)
+          value)
+         ((= address (+ memory-control-begin #x20))
+          (format t "Wrote 0x~8,'0x to common delay!~%" value)
+          value)
+         (t (error "Unexpected write of 0x~8,'0x! to Memory Control at 0x~8,'0x!~%" value address))))
+      ((= address ram-size-begin)
+       (format t "Wrote 0x~8,'0x to ram size!~%" value)
+       value)
+      ((= address cache-control)
+       (format t "Wrote 0x~8,'0x to cache control!~%" value)
+       value)
+      ; RAM
+      ((in-range ram-begin ram-size address)
+       (format t "Wrote 0x~8,'0x to ram(0x~8,'0X)!~%" value address)
+       (write-word-to-byte-array (psx-ram psx) (- address ram-begin) value))
+      ; Unimplemented.
+      (t (error "Word writes to 0x~8,'0X are unimplemented!~%" address)))))
 
 (declaim (ftype (function (psx) function) map-memory))
 (defun map-memory (psx)
